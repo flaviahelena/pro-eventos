a9f29add76cd52739d22680ac07be732c3076dc8
Flavia Helena da Silva <flaviahsilcor@gmail.com>
2023-02-27 22:54:10 -0300

    Seção 5: Angular, Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -2,23 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { SpeakersComponent } from './speakers/speakers.component';
+import { NavComponent } from './nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
-    SpeakersComponent
-  ],
+    SpeakersComponent,
+      NavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/events/events.component.ts
+++ Front/ProEventos-App/src/app/events/events.component.ts
@@ -8,18 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsComponent implements OnInit {
 
-  public events: any;
+  public events: any = [];
+  public filterEvents: any = [];
+  widthImg: number = 150;
+  marginImg: number =2;
+  showImg: boolean = true;
+  private _listFilter: string = '';
 
+  public get listFilter(): string {
+    return this._listFilter;
+  }
+  public set listFilter(value: string){
+    this._listFilter = value;
+    this.filterEvents = this._listFilter ? this.eventFilter(this.listFilter) : this.events;
+  }
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEvents();
   }
 
+  handleStatusImage(){
+    this.showImg = !this.showImg;
+  }
+
+  eventFilter(filterBy: string): any{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.events.filter(
+      (event: any) => event.theme.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      event.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    )
+  }
   public getEvents(): void {
 
     this.http.get('https://localhost:5001/api/events').subscribe(
-      response => this.events = response,
+      response => {
+        this.events = response
+        this.filterEvents = this.events
+      },
       error => console.log(error)
     );
   }
